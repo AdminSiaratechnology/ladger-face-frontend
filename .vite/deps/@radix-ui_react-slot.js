1a98cea931852017222c8b3553043347c4162c8b
Apple <apple@Apples-MacBook-Pro.local>
2025-09-05 10:59:56 +0530

    second commit

---
+++ .vite/deps/@radix-ui_react-slot.js
@@ -0,0 +1,15 @@
+import {
+  Slot,
+  Slottable,
+  createSlot,
+  createSlottable
+} from "./chunk-QG7MBTAX.js";
+import "./chunk-32E4H3EV.js";
+import "./chunk-G3PMV62Z.js";
+export {
+  Slot as Root,
+  Slot,
+  Slottable,
+  createSlot,
+  createSlottable
+};
