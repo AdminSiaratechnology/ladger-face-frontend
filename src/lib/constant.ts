a6ea97b7cf23a93e846af6b561902d2d6da9be4e
Apple <apple@Apples-MacBook-Pro.local>
2025-10-08 14:19:27 +0530

    login page change and pdf fix in company page

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,4 +1,5 @@
-const baseUrl = "http://localhost:8000/api/";
+// const baseUrl = "http://localhost:8000/api/";
 // const baseUrl = "http://51.20.248.20/api/";
+const baseUrl = "https://api.ledgerface.com/api/";
 
 export default  baseUrl;
